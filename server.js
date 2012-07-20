65fce34ee0409f8c9a912dbb1bad6057c59f57f6
libra guest <odomontois@gmail.com>
2012-07-20 14:17:01 -0400

    added Cloud9 port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'));
 module.exports = app
 
 var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
-var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+var port    = process.env.OPENSHIFT_INTERNAL_PORT || process.env.C9_PORT || 8080;
 
 app.listen(port,ipaddr, function(){
   console.log("server listening on %d in %s mode", port, app.settings.env);
